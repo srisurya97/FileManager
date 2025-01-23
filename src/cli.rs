52a3314f18632443c3460b4c1dca319e7482ad15
srisurya97 <srisurya97@github.com>
2025-01-24 01:44:05 +0530

    working on ls command. added help command

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 use crate::filesystem;
 use crate::errors;
 
-pub const SUPPORTED_COMMANDS: [&str; 3]  = [ "exit", "clear", "ls" ]; //, "cd", "rm", "mv", "cp" ];
+pub const SUPPORTED_COMMANDS: [&str; 4]  = [ "help", "exit", "clear", "ls" ]; //, "cd", "rm", "mv", "cp" ];
 
 
 struct CLI_ {
@@ -35,10 +35,35 @@ pub fn display_files_and_directories(filedirinfo: &filesystem::FileDirInfo){
 
 }
 
-pub fn display_folder_content(){
+pub fn display_folder_content(cmd_: Vec<&str>){
+    
+    let mut dir_path: String = String::new();
 
-    let current_dir_path:String = filesystem::get_current_path();
-    let files_and_directories = filesystem::get_list_of_files_and_directories(current_dir_path);
+    if cmd_.len() == 1 {
+    
+        dir_path = filesystem::get_current_path();
+        
+        println!("Current Path");
+    } else if cmd_.len() == 2 {
+        
+        if cmd_[1].trim() == ".".to_string(){
+
+            dir_path = filesystem::get_current_path();
+            println!("Current Folder: .");
+
+        } else {
+            // Possibilities
+            // /
+            // ..
+            // forward path
+
+            dir_path = filesystem::get_current_path();
+            println!("Path: {}", cmd_[1]);
+        }
+
+    }
+
+    let files_and_directories = filesystem::get_list_of_files_and_directories(dir_path);
     for each in files_and_directories {
         let filedirinfo: filesystem::FileDirInfo = each;
         display_files_and_directories(&filedirinfo)
@@ -46,32 +71,55 @@ pub fn display_folder_content(){
 
 }
 
+pub fn display_help(){
+    println!("\nSuppported Operations:\n\t{:?}\n",SUPPORTED_COMMANDS);
+}
 
 pub fn error_handle(_err: u8){
+
+    match _err {
+        errors::ERROR_UNSUPPORTED=> {
+            println!("Unsupported Operation");
+        },
+        errors::ERROR_FAILED=> {
+             println!("Failed to Execute");
+        },
+        errors::ERROR_SUCCESS=> {
+        },
+        _=> {
+            println!("Unknown Error");
+        },
+    };
+
 }
 
 
-pub fn process_cmd(cmd_to_process: String) -> u8 {
+pub fn process_cmd(cmd_to_process_: String) -> u8 {
 
-    if cmd_to_process.trim() == "".to_string() {
+    if cmd_to_process_.trim() == "".to_string() {
         return errors::ERROR_SUCCESS;
     }
 
-    const exit_cmd:&str = SUPPORTED_COMMANDS[0];
-    const clear_cmd:&str = SUPPORTED_COMMANDS[1];
-    const ls_cmd:&str = SUPPORTED_COMMANDS[2];
+    const HELP_CMD:&str = SUPPORTED_COMMANDS[0];
+    const EXIT_CMD:&str = SUPPORTED_COMMANDS[1];
+    const CLEAR_CMD:&str = SUPPORTED_COMMANDS[2];
+    const LS_CMD:&str = SUPPORTED_COMMANDS[3];
 
+    let cmd_to_process: Vec<&str> = cmd_to_process_.split_whitespace().collect();
 
-    match cmd_to_process.trim().as_ref() {
-        ls_cmd=>{
-            display_folder_content();
+    match cmd_to_process[0].trim().as_ref() {
+        LS_CMD=>{
+            display_folder_content(cmd_to_process);
         },
-        clear_cmd=>{
+        CLEAR_CMD=>{
         },
-        exit_cmd=>{
+        HELP_CMD=>{
+            display_help();
+        },
+        EXIT_CMD=>{
         },
         _=> {
-            return errors::ERROR_FAILED;
+            return errors::ERROR_UNSUPPORTED;
         },
     };
 
@@ -83,7 +131,7 @@ pub fn process_cmd(cmd_to_process: String) -> u8 {
 pub fn cli_init() {
 
     println!("CLI Init\r\n");
-    println!("Supported Commands: {:?}", SUPPORTED_COMMANDS);
+    println!("Type \"help\" for options.");
     
     let cli_ = CLI_{
       current_path: filesystem::get_current_path(),  
@@ -102,11 +150,9 @@ pub fn cli_run() {
         
         print!("FM$ ");
         io::stdout().flush().unwrap();
-        let _ = io::stdin().read_line(&mut cmd);
         
-        if process_cmd(cmd.clone()) != errors::ERROR_SUCCESS {
-            println!("Unsupported Operation");
-        }
+        let _ = io::stdin().read_line(&mut cmd);
+        error_handle(process_cmd(cmd.clone()));
         
         cmd.clear();
     }
